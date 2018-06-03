094dbc8213323b984f941eb821f2ac90769d490e
Henrique <fale.henrique@gmail.com>
2018-06-03 19:46:10 -0300

    add transfer ownership

--- app/scripts/accounAuthorizer.js
+++ app/scripts/accounAuthorizer.js
@@ -4,11 +4,10 @@
  */
 //function addAuthorizer(address _authorized, TypeAuthorizer _typeAuthorizer) public onlyOwner {
 $( "#btnAddAuthorizer" ).click(function() {
-    console.info("emtrou aui");
-    var instance = getInstanceContract();
+    let instance = getInstanceContract();
     let _authorized = $("#txtConta").val();
     let _typeAuthorizer = $("#cmbProfile").val();
-    instance.addAuthorizer.sendTransaction(_authorized, _typeAuthorizer, function(error,result){
+    instance.addAuthorizer(_authorized, _typeAuthorizer, function(error,result){
         if (error) {
             console.info(error);
         } else {        
@@ -18,20 +17,77 @@ $( "#btnAddAuthorizer" ).click(function() {
     });     
 });
 
+
 //function removeAuthorizer(address _authorized) public onlyOwner {
 $( "#btnRemoveAuthorizer" ).click(function() {
-    var instance = getInstanceContract();
+    let instance = getInstanceContract();
     let _authorized = $("#txtConta").val();
-    instance.removeAuthorizer.sendTransaction(_authorized, function(error,result){
+    instance.removeAuthorizer(_authorized, function(error,result){
         if (error) {
             console.info(error);
         } else {        
-            alert(result);
             setModal(result);
         }
     });     
 });
-    
+
+//function transferOwnership(address newOwner) public onlyOwner {
+$( "#btnTransferOwnership" ).click(function() {
+    let instance = getInstanceContract();
+    let _authorized = $("#txtContaNova").val();
+    instance.transferOwnership(_authorized, function(error,result){
+        if (error) {
+            console.info(error);
+        } else {        
+            setModal(result);
+            eventTransferOwnership();
+        }
+    });     
+});
+
+//event.stopWatching();
+function eventTransferOwnership() {
+    var instance = getInstanceContract();
+    var event = instance.OwnershipTransferred();
+    event.watch(function(error, result){
+        if (!error)
+        {
+            getOwner(function(result){
+                $("#lblOwner").text(result);
+            });            
+            console.info("dados " + result.args.newOwner);
+            event.stopWatching();
+        } else {
+            console.log(error);
+        }
+    });    
+}
+
+//mapping(address => Authorizer) public _authorizers;
+$( "#btnAuthorizers" ).click(function() {
+    let instance = getInstanceContract();
+    let _authorized = $("#txtConta").val();
+
+    instance._authorizers.call(_authorized, function(error,result){
+        if (error) {
+            console.info(error);
+        } else {        
+            let _address = result[0];
+            var entryDate = new Date(result[1] * 1000);
+            entryDate = entryDate.toUTCString();
+            let statusAuthorizer = result[2];
+            let typeAuthorizer = result[3];
+
+            $('#authorizerModal').modal('show')
+            $("#labelConta").text(_address);
+            $("#labelData").text(entryDate);
+            $("#labelStatus").text(statusAuthorizer);
+            $("#labelTipo").text(typeAuthorizer);
+ 
+        }
+    });     
+});
+
 function setModal(transaction) {
     $('#lblTransaction').text(transaction);
     $('#transactionModal').modal('show')
