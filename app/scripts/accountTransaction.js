beac1a5e813ba1c616ce23fb78a55c9dd21908c1
André Ferreira <contato@andreferreira.me>
2018-08-04 17:18:37 -0300

    Transaction modal, copy address and format date

--- app/scripts/accountTransaction.js
+++ app/scripts/accountTransaction.js
@@ -2,21 +2,109 @@
 
 $(document).ready(() => {
   pendingTransactions()
+
+  var clipboard = new ClipboardJS('.address');
+
+  clipboard.on('success', (e) => {
+    $('#' + e.trigger.id).attr("title", "Copiado!");
+    $('#' + e.trigger.id).tooltip('show')
+    setTimeout(() => {
+      $('#' + e.trigger.id).tooltip('hide')
+      $('#' + e.trigger.id).attr("title", "");
+    }, 1000)
+  })
+
+  $("#frmTransaction").validate({
+    rules: {
+      "txtTransactionID": {
+        required: true,
+        min: 1
+      }
+    },
+    messages: {
+      "txtTransactionID": {
+        required: "Campo obrigatório",
+        min: "Informe um ID maior que 0"
+      }
+    }
+  });
+})
+
+$("#btnSearch").click(() => {
+  if ($("#frmTransaction").valid()) {
+    const id = $("#txtTransactionID").val()
+    getTransactionDetail(id, (tx) => {
+      // const from = shrinkAddress(tx[0])
+      // const to = shrinkAddress(tx[1])
+      // const description = web3.toAscii(tx[2])
+      // const amount = web3.fromWei(tx[3], 'ether') + " ETH"
+      // const date = new Date(tx[4]*1000)
+      // let status = getEnumStatusTransaction(tx[7])
+
+      $('#detailsModal #signatureCountColab').text(tx[5])
+      $('#detailsModal #signatureCountAdviser').text(tx[6])
+
+      instance._signaturesColabs(id, (err, result) => {
+        if (!err) {
+          f(result)
+        } else {
+          console.log(err)
+        }
+      })
+
+      $('#detailsModal #approveVotes').text(10)
+      $('#detailsModal #declineVotes').text(10)
+    })
+  } else {
+    return false;
+  }
 })
 
 $("#btnSign").click(() => {
-  const id = $("#txtTransactionID").val()
-  signTransaction(id, (result) => {
-    console.log(result)
-  })
+  if ($("#frmTransaction").valid()) {
+    const id = $("#txtTransactionID").val()
+    signTransaction(id, (result) => {
+      setModal(result, 'SIGN')
+    })
+  } else {
+    return false;
+  }
 })
 
 $("#btnCancel").click(() => {
-  const id = $("#txtTransactionID").val()
-  deleteTransaction(id, (result) => {
-    console.log(result)
-  })
+  if ($("#frmTransaction").valid()) {
+    const id = $("#txtTransactionID").val()
+    deleteTransaction(id, (result) => {
+      setModal(result, 'CANCEL')
+    })
+  } else {
+    return false;
+  }
 })
+
+function setModal(txn, action) {
+  $('.modal-body').html(
+    '<p>Transação executada, aguarde a validação da rede...</p>' +
+    '<p>Transação: <mark id="lblTransaction" class="small"></mark></p>'
+  );
+
+  switch (action) {
+    case 'SIGN':
+      $('#transactionModalLabel').text('Transação Validada');
+      break;
+    case 'CANCEL':
+      $('#transactionModalLabel').text('Transação Cancelada');
+      break;
+
+    default:
+      break;
+  }
+
+  $('#lblTransaction').text(txn);
+  $('#transactionDetails').attr('href', 'https://rinkeby.etherscan.io/tx/'.concat(txn));
+  $('#transactionModal').modal('show');
+  $('#frmTransaction')[0].reset();
+}
 
 function pendingTransactions () {
   getPendingTransactions((transactions) => {
@@ -50,11 +138,11 @@ function pendingTransactions () {
 
           $('#pendingTransactions > table > tbody').append(
             '<tr> <th scope="row">' + id + '</th>' +
-            '<td>' + from + '</td>' + 
-            '<td>' + to + '</td>' +
+            '<td><span id="' + id + 'addressFrom" class="address text-primary" style="cursor: pointer" data-clipboard-text="' + tx[0] + '" data-trigger="manual" data-toggle="tooltip" data-placement="top">' + from + '</span></td>' +
+            '<td><span id="' + id + 'addressTo" class="address text-primary" style="cursor: pointer" data-clipboard-text="' + tx[1] + '" data-trigger="manual" data-toggle="tooltip" data-placement="top">' + to + '</span></td>' +
             '<td>' + description + '</td>' +
             '<td>' + amount + '</td>' +
-            '<td>' + date + '</td>' +
+            '<td>' + moment(date).format('DD MMM Y - hh:ss:SS A') + '</td>' +
             '<td>' + status + '</td>' +
             '</tr>'
           )
@@ -62,6 +150,10 @@ function pendingTransactions () {
       })
     }
   })
+}
+
+function getVoteValue (voteId) {
+
 }
 
 function deleteTransaction (id, f) {
