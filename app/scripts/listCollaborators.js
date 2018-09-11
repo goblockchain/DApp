90b12281ebedf88ebe93272686cfe20e744fa861
Henrique <fale.henrique@gmail.com>
2018-09-11 09:40:06 -0300

    lista de colabs

--- app/scripts/listCollaborators.js
+++ app/scripts/listCollaborators.js
@@ -1,164 +1,46 @@
 var abiIdentity = [{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"hashTerms","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"person","outputs":[{"name":"sender","type":"address"},{"name":"hashUport","type":"string"},{"name":"status","type":"uint8"},{"name":"hashTerms","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"addr","type":"address"},{"name":"roleName","type":"string"}],"name":"checkRole","outputs":[],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_uPort","type":"string"},{"name":"_approveOrDisapprove","type":"bool"}],"name":"validate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_uPort","type":"string"}],"name":"getPersonByIdUport","outputs":[{"name":"","type":"address"},{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"addr","type":"address"},{"name":"roleName","type":"string"}],"name":"hasRole","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_hash","type":"string"}],"name":"setTermsAndCondition","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_addressColab","type":"address"}],"name":"isCollaborator","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_newSuperuser","type":"address"}],"name":"transferAdmin","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getValidHash","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"addr","type":"address"},{"name":"roleName","type":"string"}],"name":"adminRemoveRole","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_uPort","type":"string"},{"name":"_hashTerms","type":"string"},{"name":"_accepted","type":"bool"}],"name":"requestApprove","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"mapPersonAddress","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"validHash","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"mapPerson","outputs":[{"name":"sender","type":"address"},{"name":"hashUport","type":"string"},{"name":"status","type":"uint8"},{"name":"hashTerms","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"ROLE_ADMIN","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getTotalListPerson","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"ROLE_COLLABORATOR","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"_uPort","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"isValid","type":"bool"},{"indexed":false,"name":"validator","type":"address"}],"name":"PersonValidate","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"idUport","type":"string"}],"name":"RequestPerson","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"sender","type":"address"},{"indexed":false,"name":"time","type":"uint256"},{"indexed":false,"name":"validHash","type":"string"}],"name":"TermsAndConditionChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"collaborator","type":"address"}],"name":"NewCollaborator","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"addr","type":"address"},{"indexed":false,"name":"roleName","type":"string"}],"name":"RoleAdded","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"addr","type":"address"},{"indexed":false,"name":"roleName","type":"string"}],"name":"RoleRemoved","type":"event"}];
 
 //rinkeby
-let contractAddressIdentity = "0xBBe5932a778698F2A8ea826B7A53F1435dBead08";
+let contractAddressIdentity = "0x6C916EeD5667429106B65FC09F8652d6266c431e";
 let instanceIdentity = getInstanceContract(abiIdentity, contractAddressIdentity);
 
-let ROLE_ADMIN = "admin";
-let ROLE_ADVISOR = "advisor";
-let ROLE_ESPECIALIST = "especialist";
-let ROLE_AMBASSADOR = "ambassador";
-let ROLE_COLLABORATOR = "collaborator";
-
 $(document).ready(() => {
-    /**
-     * Neste retorno você era receber um objeto contendo as seguintes informações
-     * { auth : true , nonce : ‘hash aleatório gerado pela plataforma’}
-     */
-    window.OMID = new OMID('01', 'stag', ['name', 'blockchainid', 'email', 'photo', 'latitude', 'longitude'], function(result){
-        if(result.auth && result.nonce){
-            callAuth(result.nonce);
-            console.info(result.nonce);
-        }
-    });
-
-    if ($("#lblColab") != undefined) {
-        $("#lblColab").text();
-    }
-    $(".divRole").hide();
-    isCollaborator();
-    isAdvisor();
     getListPerson();
 });
 
-function isCollaborator() {    
-
-    checkRole(ROLE_COLLABORATOR, function (error, result) {
-       if (error) {
-            $(".divRole").hide();
-        } else {
-            if(result) {
-                $("#divColab").show();
-            } else {
-                $("#divColab").hide();
-            }
-        }         
-    });
-
-    // return (
-    //     hasRole(_addressUser, ROLE_ADMIN) ||
-    //     hasRole(_addressUser, ROLE_ADVISOR) ||
-    //     hasRole(_addressUser, ROLE_ESPECIALIST) ||
-    //     hasRole(_addressUser, ROLE_AMBASSADOR) || 
-    //     hasRole(_addressUser, ROLE_COLLABORATOR)            
-    // );
-}
-
-function isAdvisor() {
-
-    checkRole(ROLE_ADMIN, function (error, result) {
-       if (error) {
-            console.error(error);
-        } else {
-            console.log(result);
-            if(result) {
-                $("#divColabAdvisor").show();
-            } else {
-                $("#divColabAdvisor").hide();
-            }
-        }       
-    });
-}
-
-function checkRole(role, e) {
-
-    instanceIdentity.hasRole(web3.eth.accounts[0], role, (error, result) => {
-        e(error, result);
+function getListPerson() {
+    instanceIdentity.getTotalListPerson(function(error, result) {
         if (error) {
             console.error(error);
         } else {
-            console.log(result);
+            console.log(result.toNumber());
+            for (var i = 0; i < result.toNumber(); i++) {
+                instanceIdentity.person(i, function(error, result) {
+                    if (error) {
+                        console.error(error);
+                    } else {
+                        var gbc = "-";
+                        var description ="-";
+                        var reputation = "-";
+                        var address = result[0];
+                        var uport = result[1];
+                        $('#divColabs').append(
+                            '<div class="col-sm-4">'+
+                            '<div class="card mb-3">'+
+                                '<div class="card-body">'+
+                                    '<h5 class="card-title">'+address+'</h5>'+
+                                    '<h6 class="card-subtitle mb-2 text-muted">'+reputation+'</h6>'+
+                                    '<p class="card-text">'+uport+'</p>'+
+                                    '<p class="card-text">'+description+'</p>'+
+                                    '<h5>'+gbc+'</h5>'+
+                                '</div>'+
+                            '</div>'+
+                            '</div>'
+                        );
+                        console.log(result);
+                    }
+                });
+            }
         }
-    });
+    });    
 }
-
-function getListPerson() {
-    // var search = true;
-    // var count = 0;
-    // while(search) {
-    //     instanceIdentity.person(count++, function(error, result) {
-    //         if (error) {
-    //             console.error(error);
-    //         } else {
-    //             if (result[0].length == 0) {
-    //                 search = false;
-    //             }                
-    //             console.log(result);
-    //             console.log(result[0]);
-    //             console.log(result[1]);
-    //             console.log(result[2]);                
-    //             console.log(result[3]);                 
-    //         }  
-    //     });
-    // }
-}
-
-
-function callAuth(nonce) {
-    $.ajax({
-        url : "http://localhost:8181/demo/auth/" +nonce,
-        success: callAuthSuccess,
-        error: function(result) {
-            console.error(result);
-        },
-    })
-}
-
-function callAuthSuccess(result)
-{
-    loginSession.find('span.log-user-email').html(result.email.value);
-    loginSession.find('span.log-user-name').html(result.name.value);
-    loginSession.find('span.log-user-blockchainid').html(result.blockchainid.value);
-    loginSession.find('span.log-user-mobile_lat').html(result.latitude.value);
-    loginSession.find('span.log-user-mobile_lng').html(result.longitude.value);
-
-    photoNonce = result.photo.value;
-    startSession.hide();
-    loginSession.show();
-}
-
-$("#btnSendRequest").click(function() {
-
-    let txtIDuPort = $("#txtIDuPort").val();
-    let _hashTerms = "abc";
-    let _accepted = true;
-
-    instanceIdentity.requestApprove(txtIDuPort, _hashTerms, _accepted, (err, result) => {
-        if (!err) {
-        //   setModal(result, 'WITHDRAW');
-            console.error(result);
-        } else {
-            console.error(err);
-        }
-      });
-});  
-
-
-var uportconnect = window.uportconnect
-const uport = new uportconnect.Connect('GoBlockchain dApp', {
-    clientId: '2ozaREEZXKfhCbRcdLuMYzo9Zy8nts83Ddf',
-    signer: uportconnect.SimpleSigner('549db5cdbab21a3543ea43e434eb9252c229922e3cd2e00a965e55a7acc0d9f3'),
-    network: 'rinkeby'
-})
-
-// Request credentials to login
-uport.requestCredentials({
-    requested: ['name', 'country'],
-    notifications: true // We want this if we want to recieve credentials
-}).then((credentials) => {
-    // Do something
-    console.info(credentials);
-    $("#txtIDuPort").val(credentials.address);
-    $("#txtNome").val(credentials.name);
-    console.info(credentials.country)
-})
